255034ae94eb65966b5e3bb917eac99e798ce6b0
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-26 01:19:38 -0400

    P875A02: fastbootd is now enabled by default
    Change-Id: Ieb54140550168f84ba4f067605fd6f7107979c31

--- device.mk
+++ device.mk
@@ -146,10 +146,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service.xml \
     vendor.qti.hardware.memtrack-service
 
-# Fastboot
-PRODUCT_PACKAGES += \
-    fastbootd
-
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
